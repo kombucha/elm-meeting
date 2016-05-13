e62bf10456e95876923a471a39c45cfe42ef60ea
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2016-05-13 17:22:59 +0200

    Feature: mostly functionnal (haha)

--- src/App.elm
+++ src/App.elm
@@ -1,7 +1,9 @@
-import Html exposing (..)
 import Html.App as Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import Time exposing (Time, second)
-
+import String
 
 
 main =
@@ -13,12 +15,9 @@ main =
     }
 
 
-
 -- MODEL
-
-
-type alias Person =
-  { rate : Float }
+type alias Person = Int
+type alias PersonId = Int
 
 type alias Model =
   { people : List Person
@@ -26,26 +25,20 @@ type alias Model =
   , meetingCost : Float
   }
 
-
 init : (Model, Cmd Msg)
 init =
   (Model [] False 0, Cmd.none)
 
 
-
 -- UPDATE
-
-
 type Msg
   = AddPerson
-    | SetPersonCost Int Float
-    | RemovePerson Int
+    | RemovePerson PersonId
+    | SetPersonCost PersonId String
     | StartMeeting
     | Tick Time
     | EndMeeting
     | Reset
-
-
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
@@ -53,28 +46,100 @@ update action model =
 
     AddPerson ->
       let
-        newPerson = Person 500
+        newPerson = 500
         newPeople = model.people ++ [ newPerson ]
       in
         ({model | people = newPeople}, Cmd.none)
 
-    _ ->
-      (model, Cmd.none)
+    SetPersonCost index rateAsString ->
+      let
+        newRate = Result.withDefault 0 (String.toInt rateAsString)
+        newPeopleList = updateInList index newRate model.people
+      in
+        ({model | people = newPeopleList}, Cmd.none)
 
+    RemovePerson index ->
+        ({model | people = removeFromList index model.people}, Cmd.none)
+
+    StartMeeting ->
+      ({model | meetingStarted = True}, Cmd.none)
+
+    EndMeeting ->
+      ({model | meetingStarted = False}, Cmd.none)
+
+    Tick _ ->
+      let
+        newMeetingCost = model.meetingCost + (model.people |> List.map rateBySecond |> List.sum)
+      in
+        ({model | meetingCost = newMeetingCost}, Cmd.none)
+
+    Reset ->
+      ({model | meetingCost = 0}, Cmd.none)
+
+removeFromList : Int -> List a -> List a
+removeFromList index list =
+  (List.take index list) ++ (List.drop (index+1) list)
+
+updateInList : Int -> a -> List a -> List a
+updateInList index value list =
+  (List.take index list) ++ [value] ++ (List.drop (index+1) list)
+
+rateBySecond : Int -> Float
+rateBySecond rate =
+  (toFloat rate) / 3600
+
+{-| Round a `Float` to a given number of decimal places. -}
+roundTo : Int -> Float -> Float
+roundTo places =
+  let
+    factor = 10 ^ places
+  in
+    (*) factor >> round >> toFloat >> (\n -> n / factor)
 
 
 -- SUBSCRIPTIONS
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  if model.meetingStarted then
+    Time.every second Tick
+  else
+    Sub.none
 
 
-
--- VIEW
-
-
+-- VIEWs
 view : Model -> Html Msg
 view model =
-    div [] [text "Hello World !"]
+    div []
+      [ viewMeetingCost model.meetingCost
+      , viewCommands model.meetingStarted
+      , viewPeople model.people
+      ]
+
+viewCommands : Bool -> Html Msg
+viewCommands meetingStarted =
+  div []
+    [ button [onClick AddPerson] [text "Add person"]
+    , button [(disabled meetingStarted), (onClick StartMeeting)] [text "Start"]
+    , button [(disabled (not meetingStarted)), (onClick EndMeeting)] [text "Stop"]
+    , button [(onClick Reset)] [text "Reset"]
+    ]
+
+viewMeetingCost : Float -> Html Msg
+viewMeetingCost meetingCost =
+  let
+    roundedCostString = meetingCost |> roundTo 2 |> toString
+  in
+    div [] [text roundedCostString]
+
+viewPeople : List Person -> Html Msg
+viewPeople people =
+  ul [] (List.indexedMap (\index person -> li [] [viewPerson index person]) people)
+
+viewPerson : PersonId -> Person -> Html Msg
+viewPerson personId personRate =
+  p []
+    [ text "I cost"
+    , input [(type' "number"), (value (toString personRate)), (onInput (SetPersonCost personId))] []
+    , text "Per hour"
+    , button [onClick (RemovePerson personId)] [text "x"]
+    ]
